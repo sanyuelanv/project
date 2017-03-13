69709dbb0b2fdb4a3fb18892173c7088ceef99a3
宋航 <songhang@LiMeiyundeMBP.lan>
2017-03-13 09:02:42 +0800

    webpack2

--- app/router/index.js
+++ app/router/index.js
@@ -3,18 +3,23 @@ import 'whatwg-fetch'
 import React from 'react'
 import Home from './home'
 import User from './user'
-import { Router, Route, Link , browserHistory } from 'react-router'
+import {
+  Router,
+  Route,
+  Link,
+  browserHistory
+} from 'react-router'
 
 
-class Component extends React.Component{
-	render(){
-    	return(
-            <Router history={browserHistory}>
-                 <Route path="/" component={Home} />
-                 <Route path="/user" component={User}></Route>
-            </Router>
-        )
-	}
+class Component extends React.Component {
+  render() {
+    return (
+      <Router history={browserHistory}>
+           <Route path="/" component={Home} />
+           <Route path="/user" component={User}></Route>
+      </Router>
+    )
+  }
 }
 
 export default Component
